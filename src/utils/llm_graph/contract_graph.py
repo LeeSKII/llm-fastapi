92c465b17ddc84155a1e73c984e481005f6c122d
DESKTOP-LFEE3V4\admin <leeskiikseel@outlook.com>
2025-08-14 16:18:01 +0800

    更新合同查询

--- src/utils/llm_graph/contract_graph.py
+++ src/utils/llm_graph/contract_graph.py
@@ -1,4 +1,5 @@
 from langgraph.graph import StateGraph, START, END
+from langgraph.types import Send
 from typing import TypedDict,Annotated,List,Optional,Any
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage,SystemMessage
@@ -9,6 +10,8 @@ from openai import OpenAI
 import lancedb
 import pandas as pd
 import logging
+from textwrap import dedent
+import operator
 
 import sys
 from pathlib import Path
@@ -47,6 +50,7 @@ class State(TypedDict):
     vector_search_contract:pd.DataFrame
     keyword_search_contract:pd.DataFrame
     filtered_contracts: List[Any]
+    contract_info_checked:Annotated[list[dict], operator.add]
     messages:List[dict]
     response:str
     error:str
@@ -58,7 +62,15 @@ def generate_search_words(state: State) -> State:
       llm_with_structured_output = llm.with_structured_output(SearchKeyWords).with_retry(stop_after_attempt=3)
       parser = PydanticOutputParser(pydantic_object=SearchKeyWords)
       format_instructions = parser.get_format_instructions()
-      system_prompt = "你是一个合同查询的助手，用户提出的查询需求可能是关于某些项目的也可能是关于某些设备的，请将用户的查询分解成不同的查询关键字，你只能提取用户提供的查询关键字，这意味着你绝对禁止虚拟项目或者设备关键字，只能从用户的原始查询中提取，注意，你需要提取的是关键字，这个关键字用于执行SQL的精确搜索，所以一定要保持简洁，否则会导致严重的业务问题。需要按照提供的json格式进行输出：\n{format_instructions}\n"
+      system_prompt = dedent("""你是一个合同查询的助手，用户提出的查询需求可能是关于某些项目的也可能是关于某些设备的，请将用户的查询分解成不同的查询关键字，你只能提取用户提供的查询关键字，这意味着你绝对禁止虚拟项目或者设备关键字，只能从用户的原始查询中提取，注意，你需要提取的是关键字，这个关键字用于执行SQL的精确搜索，所以一定要保持简洁，否则会导致严重的业务问题。
+      这里有一些示例：
+      1.用户提问：湖南华菱涟钢项目和揭阳大南海石化工业区危险废物焚烧以及宝山钢铁四烧结余热锅炉价格对比。
+      分析：先提取项目关键字：湖南华菱涟钢，揭阳大南海石化工业区危险废物焚烧、宝山钢铁四烧结分别是对应的项目名称，注意这里的关键词是项目名称，所以关键词是湖南华菱涟钢，并不是湖南华菱涟钢项目，关键字不需要带额外的「项目」两个字；
+      第二步检查设备关键字，这里的余热锅炉是某种工程设备，所以这里是一个设备关键字：余热锅炉。
+      2.用户提问：增压风机的采购合同有哪些
+      分析：先提取项目关键字：这里不涉及项目名称，所以不用提取；
+      第二步检查设备关键字，这里的增压风机是一种设备，所以这里是一个设备关键字：增压风机。
+      需要按照提供的json格式进行输出：\n{format_instructions}\n""")
       response = llm_with_structured_output.invoke([SystemMessage(system_prompt.format(format_instructions=format_instructions)),*state["messages"], HumanMessage(state["query"])])
       logging.info(f"generate_search_words,生成查询关键字格式化输出：{response}")
       return {"meta_search_query_keyword": response}
@@ -79,7 +91,7 @@ def vector_search(state: State)->State:
            search_results = pd.concat(df_list, ignore_index=True)
            # 根据组合列去重（保留第一个出现的值）
            final_df = search_results.drop_duplicates(subset=['contact_no', 'project_name'])
-           logging.info(f"vector_search,向量化查询结果：{final_df['contact_no', 'project_name'].to_dict(orient='records')}")
+           logging.info(f"vector_search,向量化查询结果：{final_df[['contact_no', 'project_name']].to_dict(orient='records')}")
            return {"vector_search_contract": final_df}
        else:
             return {"vector_search_contract": None}
@@ -96,7 +108,7 @@ def keyword_search(state: State)->State:
         
         for word in merged_list:
             logging.info(f"keyword_search,开始进行关键字查询：查询语句：doc LIKE '%{word}%'")
-            search_like_results = table.search().where(f"doc LIKE '%{word}%'").limit(5).to_pandas()
+            search_like_results = table.search().where(f"doc LIKE '%{word}%'").limit(10).to_pandas()
             logging.info(f"keyword_search,开始进行关键字查询：查询结果：{len(search_like_results)}")
             df_list.append(search_like_results)
 
@@ -112,23 +124,38 @@ def filter_contracts(state: State)->State:
     '''进行合同的过滤处理'''
     try:
         # 合并两个来源的df，排除重复数据
-        final_df = pd.concat([state["vector_search_contract"],state["keyword_search_contract"]], ignore_index=True)
+        final_df = pd.concat([state["keyword_search_contract"],state["vector_search_contract"]], ignore_index=True)
         logging.info(f"filter_contracts,合并两项查询，条目数：{len(final_df)}")
         final_df = final_df.drop_duplicates(subset=['contact_no', 'project_name'])
         logging.info(f"filter_contracts,取消重复数据，条目数：{len(final_df)}")
-        # LLM过滤掉不属于有效查询范围的合同 TODO
         return {"filtered_contracts": final_df.to_dict(orient="records")}
     except Exception as e:
         return {"filtered_contracts": [],"error":"filter_contracts.\n"+str(e)}
+    
+def continue_check_contract_belong(state: State):
+    '''检查合同是否属于用户查询范围'''
+    return [Send("check_contract_belong", {"query": state["query"], "contract_meta": s["contract_meta"], "equipment_table": s["equipment_table"]}) for s in state['filtered_contracts']]
 
+def check_contract_belong(state: dict)->State:
+    '''检查合同是否属于用户查询范围'''
+    query = state["query"]
+    contract_info = state['contract_meta']+state['equipment_table']
+    try:
+        system_prompt = f"请根据用户提供的合同信息和用户提出的问题，判断合同是否属于用户查询范围，输出false表示不属于，true表示属于，记住，只需要输出true或false，不要输出其它任何信息。"
+        llm = ChatOpenAI(model="qwen-plus",api_key=qwen_api_key,base_url=qwen_base_url,temperature=0.01)
+        response = llm.invoke([SystemMessage(system_prompt), HumanMessage(f"用户提供的合同信息：{contract_info},\n用户提出的问题：{query}")])
+        if "true" in response.content.lower():
+            return {"contract_info_checked": [{"contract_meta":state['contract_meta'],"equipment_table":state['equipment_table']}]}
+        else:
+            return {"contract_info_checked": []}
+    except Exception as e:
+        return {"error":"check_contract_belong.\n"+str(e)}
+    
 def generate_response(state: State)->State:
     try:
-        contract_list = []
-        if state["filtered_contracts"] is not None and len(state["filtered_contracts"]) > 0:      
-            for contract in state["filtered_contracts"]:
-                contract_list.append(f"合同元数据：{contract['contract_meta']}\n合同设计设备:{contract['equipment_table']}\n============\n")
+        contract_list = state["contract_info_checked"]
         logging.info(f"generate_response,最终参与合同筛选的合同数据：{contract_list}")
-        system_prompt = f"请根据以下合同信息回答用户提出的问题，请注意，严格参考合同信息，严禁虚构任何消息：\n{contract_list}\n"
+        system_prompt = f"请根据以下合同信息回答用户提出的问题，请注意，严格参考合同信息，尽量不要遗漏提供的合同信息，因为这些合同已经由上游检测程序校准过，确认属于用户的询问范围，严禁虚构任何消息：\n{contract_list}\n"
         llm = ChatOpenAI(model="qwen-plus",api_key=qwen_api_key,base_url=qwen_base_url,temperature=0.01)
         response = llm.invoke([SystemMessage(system_prompt),*state["messages"], HumanMessage(state["query"])])
         logging.info(f"generate_response,最终回复：{response.content}")
@@ -143,13 +170,15 @@ workflow.add_node("vector_search", vector_search)
 workflow.add_node("keyword_search", keyword_search)
 workflow.add_node("filter_contracts", filter_contracts)
 workflow.add_node("generate_response", generate_response)
+workflow.add_node("check_contract_belong", check_contract_belong)
 
 workflow.add_edge(START, "generate_search_words")
 workflow.add_edge("generate_search_words", "vector_search")
 workflow.add_edge("generate_search_words", "keyword_search")
 workflow.add_edge("vector_search", "filter_contracts")
 workflow.add_edge("keyword_search", "filter_contracts")
-workflow.add_edge("filter_contracts", "generate_response")
+workflow.add_conditional_edges("filter_contracts", continue_check_contract_belong)
+workflow.add_edge("check_contract_belong", "generate_response")
 workflow.add_edge("generate_response", END)
 
 
@@ -158,7 +187,7 @@ graph = workflow.compile()
 
 if __name__ == "__main__":
     # 测试用例
-    query = "热电偶采购价格"
+    query = "山东永锋余热发电、河北东海特钢项目余热锅炉价格对比。"
     state = {"query": query,"messages":[]}
     result = graph.invoke(state)
     print(result)
