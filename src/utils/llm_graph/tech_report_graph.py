98f128b430142d22ecb51aa367aab9484392cba6
leeski <540470452@qq.com>
2025-08-28 11:08:54 +0800

    add logging

--- src/utils/llm_graph/tech_report_graph.py
+++ src/utils/llm_graph/tech_report_graph.py
@@ -8,6 +8,7 @@ from typing_extensions import TypedDict
 from langgraph.types import Send
 import operator
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
@@ -47,6 +48,8 @@ def get_report(state: TechReportState):
         json=json_data,  # 自动设置 Content-Type: application/json
         timeout=10.0  # 可选：设置超时
     )
+    
+    logging.info(f"请求科技简报数据，请求地址: {url}, 请求参数: {json_data}, 状态码: {response.status_code}, 响应内容: {response.json()}")
 
     # 处理响应
     if response.status_code == 200:
